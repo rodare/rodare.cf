543242a9b07e623ddb6787145541f90419035f61
Rodrigo <rodare.cl@gmail.com>
2020-11-16 13:26:21 +0100

    a1

--- cursos/spanish/main.js
+++ cursos/spanish/main.js
@@ -87,3 +87,11 @@ function toggleContent11() {
   contentId.style.display == "block" ? contentId.style.display = "none" : 
 contentId.style.display = "block"; 
 }
+
+function toggleContent12() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content12");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
