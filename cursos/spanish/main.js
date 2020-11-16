2fbae1e7273d5154b9f5ff1017955421dd40a359
Rodrigo <rodare.cl@gmail.com>
2020-11-16 15:18:14 +0100

    flippity

--- cursos/spanish/main.js
+++ cursos/spanish/main.js
@@ -119,3 +119,11 @@ function toggleContent15() {
   contentId.style.display == "block" ? contentId.style.display = "none" : 
 contentId.style.display = "block"; 
 }
+
+function toggleContent16() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content16");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
